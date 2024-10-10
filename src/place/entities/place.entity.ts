72b66ea118498fd24f2bf4b3cf75953aef14fecc
Heeyeon Lee <up2kid@gmail.com>
2024-10-10 15:17:53 +0900

    🚩 Place 엔티티 생성

--- src/place/entities/place.entity.ts
+++ src/place/entities/place.entity.ts
@@ -2,13 +2,17 @@ import {
   AutoIncrement,
   Column,
   ForeignKey,
+  HasMany,
   Model,
   PrimaryKey,
   Table,
 } from 'sequelize-typescript';
 import { PlaceBrand } from '../../place-brand/entities/place-brand.entity';
+import { Climb } from '../../climb/entities/climb.entity';
+import { BoulderGrade } from '../../boulder-grade/entities/boulder-grade.entity';
+import { LeadGrade } from '../../lead-grade/entities/lead-grade.entity';
 
-@Table
+@Table({ tableName: 'Place_TB' })
 export class Place extends Model<Place> {
   @PrimaryKey
   @AutoIncrement
@@ -22,6 +26,21 @@ export class Place extends Model<Place> {
   address: string;
 
   @ForeignKey(() => PlaceBrand)
-  @Column({ allowNull: false })
+  @Column({ allowNull: false, field: 'place_brand_id' })
   placeBrandId: number;
+
+  @HasMany(() => Climb)
+  climbs: Climb[];
+
+  @HasMany(() => BoulderGrade)
+  boulderGrades: BoulderGrade[];
+
+  @HasMany(() => LeadGrade)
+  leadGrades: LeadGrade[];
+
+  @Column({ allowNull: false, field: 'created_at' })
+  createdAt: Date;
+
+  @Column({ allowNull: false, field: 'updated_at' })
+  updatedAt: Date;
 }
--- src/place/place.module.ts
+++ src/place/place.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { PlaceService } from './place.service';
 import { PlaceController } from './place.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { User } from '../user/entities/user.entity';
-import { Grade } from '../grade/entities/grade.entity';
 import { Place } from './entities/place.entity';
 
 @Module({
   imports: [SequelizeModule.forFeature([Place])],
   controllers: [PlaceController],
   providers: [PlaceService],
+  exports: [PlaceService],
 })
 export class PlaceModule {}
