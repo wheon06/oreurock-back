1813de41780859edb9ee40e948e3efbaa63bf442
Heeyeon Lee <up2kid@gmail.com>
2024-10-10 15:18:30 +0900

    🚩 가입 로직 수정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { AuthGuard } from '../auth/security/auth.guard';
 import { SaveUserDto } from './dto/save-user-dto';
 
 @Controller('user')
@@ -8,13 +7,12 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('join')
-  async join(@Body() dto: SaveUserDto) {
+  async signup(@Body() dto: SaveUserDto) {
     return await this.userService.save(dto);
   }
 
-  @UseGuards(AuthGuard)
   @Get(':username')
-  async join2(@Param('username') username: string) {
-    return await this.userService.findByUsername(username);
+  async validateUsername(@Param('username') username: string) {
+    await this.userService.validateUsername(username);
   }
 }
